97cedda6c136e4c69abf780817e42a413badadc9
baldwinjj <baldwinjj@g.cofc.edu>
2016-09-11 14:33:44 -0400

    Add the thunk action for retrieving sample details.

--- mars-client/src/constants.js
+++ mars-client/src/constants.js
@@ -5,5 +5,5 @@ export const BASE_URL = 'https://sesardev.geosamples.org' //the test base URL
 export const USER_SAMPLES_ENDPOINT = '/samples/user_code/'
 export const USER_VERIFICATION_ENDPOINT = '/webservices/credentials_service_v2.php'
 // Legacy endpoint since the new one appears not to work for json data
-export const FETCH_SAMPLE_IGSN_ENDPOINT = '/webservices/display.php'
+export const FETCH_SAMPLE_ENDPOINT = '/webservices/display.php'
 export const POST_SAMPLES_ENDPOINT = '/webservices/upload.php'
--- mars-client/test/sampleDetailSpecs.js
+++ mars-client/test/sampleDetailSpecs.js
@@ -65,6 +65,96 @@ describe('Fetch Sample by IGSN', () => {
       const nextState = reducer(state, action)
       expect(nextState).to.equal(Map(sample))
     })
+
+  })
+
+  describe('Asynchronous thunk action', () => {
+
+    //Configuration for async tests:
+    const middlewares = [thunk]
+    const mockStore = configureMockStore(middlewares)
+    afterEach(() => {
+      nock.cleanAll()
+    })
+
+    it ('creates an action for the successful retrieval of a sample by its IGSN', () => {
+      const sample = require('./testData/sampleDetails.json')
+      nock(constants.BASE_URL)
+        .get(constants.FETCH_SAMPLE_ENDPOINT + '?igsn=TIN000001')
+        .reply(200, {sample})
+
+      const expectedActions = [
+        {type: types.FETCH_SAMPLE_REQUEST},
+        {type: types.FETCH_SAMPLE_SUCCESS, sample}
+      ]
+      const store = mockStore({})
+
+      return store.dispatch(actions.fetchSample('gneiss@gmail.com', 'i<3Rocks', 'TIN000001'))
+        .then(() => expect(store.getActions()).to.eql(expectedActions))
+    })
+
+    it('creates an action for an invalid request with an invalid IGSN', () => {
+      nock(constants.BASE_URL)
+        .get(constants.FETCH_SAMPLE_ENDPOINT + '?igsn=TIN01')
+        .reply(400, {error: "No IGSN Supplied"})
+
+        const expectedActions = [
+          {type: types.FETCH_SAMPLE_REQUEST},
+          {type: types.FETCH_SAMPLE_FAILURE, error: 'Bad Request - IGSN is invalid.'}
+        ]
+        const store = mockStore({})
+
+        return store.dispatch(actions.fetchSample('gneiss@gmail.com', 'i<3Rocks', 'TIN01'))
+          .then(() => expect(store.getActions()).to.eql(expectedActions))
+    })
+
+    // The API does not appear to ever produce this response. Instead it always returns the 400
+    it('creates an action for an invalid request with an invalid IGSN', () => {
+      nock(constants.BASE_URL)
+        .get(constants.FETCH_SAMPLE_ENDPOINT)
+        .reply(403, {error: "No IGSN Supplied"})
+
+        const expectedActions = [
+          {type: types.FETCH_SAMPLE_REQUEST},
+          {type: types.FETCH_SAMPLE_FAILURE, error: 'Forbidden - IGSN has either been deactivated or non public.'}
+        ]
+        const store = mockStore({})
+
+        return store.dispatch(actions.fetchSample('gneis@gmail.com', 'i<3Rocks'))
+          .then(() => expect(store.getActions()).to.eql(expectedActions))
+    })
+
+    it('creates an action for the attempted retrieval of a nonexistant sample', () => {
+      nock(constants.BASE_URL)
+        .get(constants.FETCH_SAMPLE_ENDPOINT + '?igsn=TIN000004')
+        .reply(404, {sample: {error: "TIN000004 does not exist in the database"}})
+
+        const expectedActions = [
+          {type: types.FETCH_SAMPLE_REQUEST},
+          {type: types.FETCH_SAMPLE_FAILURE, error: 'Not Found - IGSN not found.'}
+        ]
+        const store = mockStore({})
+
+        return store.dispatch(actions.fetchSample('gneis@gmail.com', 'i<3Rocks', 'TIN000004'))
+          .then(() => expect(store.getActions()).to.eql(expectedActions))
+    })
+
+    it('creates a failure action when there is a network error', () => {
+      nock(constants.BASE_URL)
+        .get(constants.USER_SAMPLES_ENDPOINT + 'TIN')
+        .replyWithError('Can\'t complete the request.')
+
+        const error = 'Network connectivity error. Check your network connection.'
+
+        const expectedActions = [
+          {type: types.FETCH_SAMPLE_REQUEST},
+          {type: types.FETCH_SAMPLE_FAILURE, error}
+        ]
+        const store = mockStore({})
+
+        return store.dispatch(actions.fetchSample('TIN'))
+          .then(() => expect(store.getActions()).to.eql(expectedActions))
+    })
   })
 
 })
