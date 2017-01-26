9cd780719eb9729e4efbcff18545b29202a7dbaa
Joey Baldwin <baldwinjj@g.cofc.edu>
2017-01-25 22:46:43 -0500

    Update configureStore, to utilize the naming pattern established for reducers in the app.

--- mars-client/src/configureStore.js
+++ mars-client/src/configureStore.js
@@ -1,13 +1,14 @@
 import {createStore, applyMiddleware, compose} from 'redux'
 import thunkMiddleware from 'redux-thunk'
 import createLogger from 'redux-logger'
-import rootReducer from './reducers'
+
+import {reducer} from './mars'
 
 const loggerMiddleware = createLogger()
 
 export default function configureStore(initialState) {
   return createStore(
-    rootReducer,
+    reducer,
     initialState,
     compose(
       applyMiddleware(
