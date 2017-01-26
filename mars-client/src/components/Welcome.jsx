990ce4a486d878b591b3a3e8617f8726d0fcb542
Joey Baldwin <baldwinjj@g.cofc.edu>
2017-01-25 22:57:11 -0500

    Remove files from the previous iteration. MARS has been reorganized such that folders form a heirarchical structure akin to the actual structure of the React application, rather than an organization by the nature of the files.

--- mars-client/src/components/Welcome.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Welcome = () => (
-  <div>
-    <h2>Welcome</h2>
-  </div>
-)
-
-export default Welcome
--- mars-client/src/core/sampleDetailCore.js
+++
@@ -1,7 +0,0 @@
-import {Map} from 'immutable'
-
-export const SAMPLE_DETAIL_INITIAL_STATE = Map({})
-
-export function addSampleDetail(state, sample) {
-  return Map(sample)// we could add multiple samples by igsn like state.set(sample.igsn, sample)
-}
