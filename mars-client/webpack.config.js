0ec9e88bd071eb6ae81268d8792b3145fa1f2424
Joey Baldwin <baldwinjj@g.cofc.edu>
2017-01-25 22:30:46 -0500

    Update webpack loaders to handle Font Awesome font formats. Also, change the output path to match that of the entry name.

--- mars-client/webpack.config.js
+++ mars-client/webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'dist'),
     publicPath: "/",
-    filename: "bundle.js"
+    filename: "[name].js"
   },
   devtool: 'eval-source-map',
   devServer: {
@@ -26,6 +26,21 @@ module.exports = {
         test: /\.json?$/,
         exclude: /node_modules/,
         loader: 'json-loader'
+      }, {
+        test: /\.css$/,
+        loaders:[
+          'style?sourceMap',
+          'css?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]'
+        ]
+      }, {
+        test: /\.jpg$/,
+        loader: 'file-loader'
+      }, {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "url-loader?limit=10000&mimetype=application/font-woff"
+      }, {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "file-loader"
       }
     ]
   },
