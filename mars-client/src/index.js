be141e0d5ac5ad937b2d224a50297942be89718e
Joey Baldwin <baldwinjj@g.cofc.edu>
2017-01-25 22:44:30 -0500

    Update index.js to manage persistant state with local storage, give it the responsibility of loading global css, set up the state, set up the routes, and render the app in the DOM.

--- mars-client/src/index.js
+++ mars-client/src/index.js
@@ -1,12 +1,43 @@
-import 'babel-polyfill'
+import configureStore from './configureStore'
+import {Map} from 'immutable'
+import {Provider} from 'react-redux'
 import React from 'react'
 import {render} from 'react-dom'
-import Root from './Root'
-import configureStore from './configureStore'
+import {Router, hashHistory} from 'react-router'
 
-const store = configureStore()
+import {localState, saveState} from './helpers/localStorage'
+import routes from './mars/routes'
 
-render(
-  <Root store={store} />,
+//setup global css
+import Normalize from 'normalize.css'
+import FontAwesome from 'font-awesome-webpack'
+
+// get persistant state from local storage
+const persistedState = localState()
+if(persistedState) {
+  persistedState.user = Map(persistedState.user)
+}
+
+const store = configureStore(persistedState)
+
+// update localStorage with persistant elements
+store.subscribe(() => {
+  saveState({
+    user: {
+      username: store.getState().user.get('username'),
+      password: store.getState().user.get('password'),
+      usercode: store.getState().user.get('usercode')
+    }
+  })
+})
+
+const App = () => (
+  <Provider store={store}>
+  <Router history={hashHistory} routes={routes(store)}/>
+  </Provider>
+)
+
+render (
+  <App/>,
   document.getElementById('app')
 )
