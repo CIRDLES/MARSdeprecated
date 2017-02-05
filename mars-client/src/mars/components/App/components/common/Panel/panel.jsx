260f370e8081ffb45d6d56992a388234496209fd
baldwinjj <baldwinjj@g.cofc.edu>
2017-02-05 16:51:55 -0500

    Update Panel so that it can be given custom inline styles. This is so the height can be set when necessary. A more robust solution may be needed at another point.

--- mars-client/src/mars/components/App/components/common/Panel/panel.jsx
+++ mars-client/src/mars/components/App/components/common/Panel/panel.jsx
@@ -1,15 +1,17 @@
 import CSSModules from 'react-css-modules'
 import React from 'react'
 
-import styles from './panel.css'
+import defaultStyles from './panel.css'
 
-const Panel = ({children, name, styleName}) => (
-  <div styleName={styleName || 'panel'}>
+const Panel = ({children, name, style}) => {
+
+  return (
+  <div styleName={'panel'} style={style}>
     <h1 styleName='header'>{name}</h1>
     <div styleName='content'>
       {children}
     </div>
   </div>
-)
+)}
 
-export default CSSModules(Panel, styles)
+export default CSSModules(Panel, defaultStyles)
