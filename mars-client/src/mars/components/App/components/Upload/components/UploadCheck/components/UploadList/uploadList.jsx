57316a0003de95d3cb9e90a64729bbb6c2bae01b
baldwinjj <baldwinjj@g.cofc.edu>
2017-04-03 16:56:06 -0400

    Update list with a filter so that the list can display different features of the samples.Fix some minor styling bugs.

--- mars-client/src/mars/components/App/components/Upload/components/UploadCheck/components/UploadList/uploadList.jsx
+++ mars-client/src/mars/components/App/components/Upload/components/UploadCheck/components/UploadList/uploadList.jsx
@@ -1,20 +1,21 @@
 import CSSModules from 'react-css-modules'
 import React from 'react'
+import {Link} from 'react-router'
 
 import styles from './uploadList.css'
 import Panel from '../../../../../common/Panel'
 
-var test = []
-for (var i=0; i<25; i++) {
-  test.push(<p key={i}>test</p>)
+// later a user might have the option of filtering by different values
+const filterBy = (samples, filter) => {
+  return samples.map((sample) => sample.find((field) => field.key == filter).value)
 }
 
 const UploadList = ({uploadSamples}) => (
   <div styleName='uploadList'>
     <Panel name='Samples' style={{height: '100%'}}>
       <div>
-        {uploadSamples.map((sample, i) => (
-          <p key={i}>{sample.name.value}</p>
+        {filterBy(uploadSamples, 'name').map((sample, i) => (
+          <Link styleName='listItem' activeClassName={styles.active} key={i} to={'/check/' + i}>{sample}</Link>
         ))}
       </div>
     </Panel>
