fbb7f2ce03479d2756a3022cc906839016c95c04
Tia Curry <tiaanicolee@gmail.com>
2017-05-07 23:14:28 -0400

    adding Nav

--- mars-client/src/mars/components/Home/home.jsx
+++ mars-client/src/mars/components/Home/home.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 import CSSModules from 'react-css-modules'
 import styles from './home.css'
 
+import Nav from './components/Nav'
+
 const Home = ({children, user}) => (
   <div styleName='home'>
+    <Nav/>
       {React.cloneElement(children, user)}
   </div>
 )
