716ce893562a793b50188848762342919ef014d3
Tia Curry <tiaanicolee@gmail.com>
2017-05-07 23:05:01 -0400

    Removed Dashboard page

--- mars-client/src/mars/components/Home/home.jsx
+++ mars-client/src/mars/components/Home/home.jsx
@@ -2,48 +2,8 @@ import React from 'react'
 import CSSModules from 'react-css-modules'
 import styles from './home.css'
 
-//import Nav from './components/Nav'
-import NavBar from '../common/NavBar'
-
-var navData = [
-  {
-  "text": "Link 1",
-  "url": "#"
-  },
-  {
-  "text": "Link 2",
-  "url": "#"
-  },
-  {
-  "text": "Link 3",
-  "url": "#",
-  "submenu": [
-    {
-      "text": "Sublink 1",
-      "url": "#",
-      "submenu": [
-        {
-          "text": "SubSublink 1",
-          "url": "#"
-        }
-      ]
-    },
-    {
-      "text": "Sublink 2",
-      "url":"#",
-      "submenu": [
-        {
-          "text": "SubSublink 2",
-          "url": "#"
-        }
-      ]
-    }
-  ]
-  }
-]
 const Home = ({children, user}) => (
   <div styleName='home'>
-    <NavBar items = {navData}/>
       {React.cloneElement(children, user)}
   </div>
 )
