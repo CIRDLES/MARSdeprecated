e04e57124188753b5a4775486e355948b84e49a1
Tia Curry <tiaanicolee@gmail.com>
2017-05-10 15:20:37 -0400

    Updated About page

--- mars-client/src/mars/components/Home/components/About/about.jsx
+++ mars-client/src/mars/components/Home/components/About/about.jsx
@@ -4,7 +4,7 @@ import styles from './about.css'
 
 const About = () => (
   <div styleName='about'>
-    <div styleName='logo'></div>
+    <h1>About</h1>
     <p>
       Middleware for Assisting the Registration of Samples (MARS) is a project
       of Working Group 4 of the Internet of Samples (iSamples) NSF-funded
@@ -12,7 +12,9 @@ const About = () => (
       the automation of registering legacy samples at SESAR (System for Earth
       Sample Registration) with pertinent metadata and a IGSN (International
       GeoSample Number). The initial targeted repository is the cores collection
-      of Scripps Institution of Oceanography.
+      of Scripps Institution of Oceanography. MARS is maintained by the College
+      of Charleston with the support of the Lamont-Doherty Earth Observatory at
+      Columbia University.
     </p>
   </div>
 )
