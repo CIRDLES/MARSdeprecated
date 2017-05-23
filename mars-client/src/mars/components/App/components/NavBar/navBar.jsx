8e2e57cb4cbc5ca3438d84f3e4faa6f69d372b4c
Tia Curry <tiaanicolee@gmail.com>
2017-05-23 19:33:50 -0400

    removed secondary menu items

--- mars-client/src/mars/components/App/components/NavBar/navBar.jsx
+++ mars-client/src/mars/components/App/components/NavBar/navBar.jsx
@@ -4,23 +4,10 @@ import CSSModules from 'react-css-modules'
 import styles from './navBar.css'
 
 class NavBar extends Component{
-
     render() {
-        const secondaryMenuItems = (
-            this.props.secondaryMenuItems ?
-
-            <MenuItems
-                ref="secondaryMenuItems"
-                secondary={true}
-                items={this.props.secondaryMenuItems}/> :
-
-            null
-        );
-
         return <nav>
           <ul styleName = "navbar">
               <MenuItems ref="menuItems" styleName="menu-items" items={this.props.menuItems}/>
-              {secondaryMenuItems}
           </ul>
         </nav>;
     }
